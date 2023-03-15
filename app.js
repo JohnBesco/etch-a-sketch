6467ad69baafb43be203d05c467948199ed63f90
John Besco <johnbesco.dev@gmail.com>
2023-03-15 23:11:14 +0000

    Add hover function

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 let container = document.getElementById("container")
-let newDiv = document.createElement("div");
 
-newDiv.className = "square-div"
-container.appendChild(newDiv);
+for (let i = 0; i < 182; i++) {
+    let newDiv = document.createElement("div");
+    newDiv.className = "square-div"
+    
+    newDiv.addEventListener("mouseover", function() {
+        this.classList.add("hovered");
+    });
+    
+    container.appendChild(newDiv);
+}
 
+  
