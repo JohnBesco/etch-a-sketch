da3e6e5d2648902748976580f1cdfe0bd834c923
John Besco <johnbesco.dev@gmail.com>
2023-03-23 21:37:43 +0000

    Change javascript event listner so user only has to click once

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
-let canvas = document.getElementById("canvas")
+let canvas = document.getElementById("canvas");
+let isClicked = false;
 
 for (let i = 0; i < 1802; i++) {
-    let newDiv = document.createElement("div");
-    newDiv.className = "square-div"
-    
-    newDiv.addEventListener("mouseover", function() {
-        this.classList.add("hovered");
-    });
-    
-    canvas.appendChild(newDiv);
-}
+  let newDiv = document.createElement("div");
+  newDiv.className = "square-div";
 
+  newDiv.addEventListener("click", function() {
+    isClicked = !isClicked;
+    console.log('Clicked!');
+  });
+
+  newDiv.addEventListener("mouseover", function() {
+    if (isClicked) {
+      this.classList.add("hovered");
+    }
+  });
+
+  canvas.appendChild(newDiv);
+}
 let resetBtn = document.getElementById("reset-btn");
 
 resetBtn.addEventListener("click", function() {
